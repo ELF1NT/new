32a6204c5b5d9ea8be561f62db511923e3cc0cb4
ELF1NT <135104807+ELF1NT@users.noreply.github.com>
2024-02-06 22:22:44 +0600

    app.js

--- js/app.js
+++ js/app.js
@@ -14,6 +14,14 @@ let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 let btn7 = document.getElementById("btn7");
+
+let btn8 = document.getElementById("btn8");
+let btn9 = document.getElementById("btn9");
+let btn10 = document.getElementById("btn10");
+let btn11 = document.getElementById("btn11");
+let btn12= document.getElementById("btn12");
+let btn13 = document.getElementById("btn13");
+let btn14 = document.getElementById("btn14");
 //первый ряд
 btn1.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
@@ -91,8 +99,85 @@ btn7.addEventListener("click", function(){
 		tg.MainButton.show();
 	}
 });
+//второй ряд
 
 
+btn8.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали Место 1 в 2 ряду!");
+		item = "8";
+		tg.MainButton.show();
+	}
+});
+
+btn9.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали Место 2 в 2 ряду!");
+		item = "9";
+		tg.MainButton.show();
+	}
+});
+
+btn10.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали Место 3 в 2 ряду!");
+		item = "10";
+		tg.MainButton.show();
+	}
+});
+
+btn11.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали Место 4 в 2 ряду!");
+		item = "11";
+		tg.MainButton.show();
+	}
+});
+
+btn12.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали Место 5 в 2 ряду!");
+		item = "12";
+		tg.MainButton.show();
+	}
+});
+
+btn13.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали Место 6 в 2 ряду!");
+		item = "13";
+		tg.MainButton.show();
+	}
+});
+
+btn14.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали Место 7 в 2 ряду!");
+		item = "14";
+		tg.MainButton.show();
+	}
+});
 
 
 
